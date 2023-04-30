5cde2d263febe3074912163393d04c547f9b73c2
MrAgentRBLX <131639463+MrAgentRBLX@users.noreply.github.com>
2023-04-30 13:34:23 +0200

    Update and rename .gitignore to source.lua

---
+++ source.lua
@@ -0,0 +1,46 @@
+-- SERVICES --
+local TeleportService = game:GetService("TeleportService")
+local RunService = game:GetService("RunService")
+local Players = game:GetService("Players")
+
+
+-- MODULE --
+local module = table.create(1)
+module.__index = module
+
+
+function module:FindChild(Instance, Instance_Name, Recursively)
+   local Parent, Object, FromDescendants = Instance, nil, Recursively == true or false
+   for _, v in next, Parent:GetDescendants() do
+       if v.Name == Instance_Name then
+          Object = v
+       end
+   end
+   return Object, typeof(Object) == "Instance" or Object ~= nil
+end
+
+
+function module:IsApart(Parent, Instance_Name)
+   for _, v in next, Parent:GetChildren() do
+       if v.Parent == Instance_Name then
+          return true
+       end
+   end
+   return false
+end
+
+
+function module:Teleport(id)
+    TeleportService:Teleport(id or game.PlaceId)
+end
+
+
+function module:Then(func)
+   if not type(func) == "function" then
+       return;
+   end
+   func()
+end
+
+
+return module
